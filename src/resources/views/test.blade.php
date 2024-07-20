b14d1be112eb60af00cd72a32ce9a547b3c8029d
NatsukiHayashi <natsuki.hayashi@arsaga.jp>
2024-07-20 10:23:58 +0900

    :construction: テスト用の画面を修正

--- src/resources/views/test.blade.php
+++ src/resources/views/test.blade.php
@@ -7,10 +7,11 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    @vite(['resources/scss/app.css'])
+    @vite(['resources/scss/app.scss'])
 </head>
 
 <body>
-    <h1 class="h1">これはテストです</h1>
+    <h1 class="test">これはテストです</h1>
+    <h2 class="test">h2です。</h2>
 </body>
 </html>
